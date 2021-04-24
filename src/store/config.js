75d9d821a5dab422000a5351af9f8234678216bc
Denis Fadeev <denis@fadeev.org>
2021-04-24 23:47:11 +0500

    init

---
+++ src/store/config.js
@@ -0,0 +1,11 @@
+import { env, starport, blocks, wallet, transfers } from "@starport/vuex";
+import generated from "./generated";
+export default function init(store) {
+  for (const moduleInit of Object.values(generated)) {
+    moduleInit(store);
+  }
+  transfers(store);
+  blocks(store);
+  env(store);
+  wallet(store);
+}

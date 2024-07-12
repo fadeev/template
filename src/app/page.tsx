ce2d20926e0778bb120fd1bd395d1c52f0d37ca7
Denis Fadeev <denis@fadeev.org>
2024-07-12 14:43:32 +0300

    wip

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+function Page() {
+  return (
+    <div>
+      <ConnectButton />
+    </div>
+  );
+}
+
+export default Page;
